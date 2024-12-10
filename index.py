2b20e7a2f3650c9499fe70792748315401d52f4b
GURUCHARAN P <145788349+dearguru07@users.noreply.github.com>
2024-12-10 20:31:03 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+# print('Hello world.....')
+
+# Nuumber=int(input('Enter a number'))
+# string=str(Nuumber)
+# length=len(string)
+
+m=int(input('Nenter a nymber'))
+string=str(m)
+# print(string)
+length=len(string)
+# print(length)
+last=string(len-1)
+print(last)
